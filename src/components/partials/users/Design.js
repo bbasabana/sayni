f38be3da558b7113ff825f6f66536286764fae9d
bbasabana <bbasabana@gmail.com>
2020-06-05 13:44:17 +0200

    design to en

--- src/components/partials/users/Design.js
+++ src/components/partials/users/Design.js
@@ -24,7 +24,7 @@ class Design extends Component {
             <div className="image">
             <div className="flex items-center mb-4">
                 <div className="bg-red-600 border-2 border-blue px-3 py-2 rounded-full font-bold text-blue mr-2">5</div>
-                <h2 className="text-lg title-bloc">Faites les choix des Couleurs et les Fonts</h2>
+                <h2 className="text-lg title-bloc">Choose the Colors and Fonts</h2>
             </div>
             <div className="form-info bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-8 flex flex-col">
                 <form className="w-full max-w-lg">
@@ -42,7 +42,7 @@ class Design extends Component {
                      </div>
                     <div className="w-full md:w-1/2 px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                            Couleur
+                        Color
                         </label>
                         <GithubPicker
                           color={color} 
@@ -53,7 +53,7 @@ class Design extends Component {
                 </form>
             </div>
             <Link  to="/installation" className="bg-black-600 text-white font-bold py-4 px-6 rounded-full next-button">
-                 Suivant
+                 Next
             </Link>
         </div>
         )
